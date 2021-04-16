4e87fc559d63af7d335ad0426a4b6a56a39682bd
Eddie Cheng <54136390+chengr4@users.noreply.github.com>
2021-04-16 23:02:00 +0800

    update error page

--- src/views/ErrorPage.js
+++ src/views/ErrorPage.js
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
+
 const ErrorPage = () => {
   return ( 
-    <h2>Error</h2>
+    <div className="not-found">
+      <h2>Sorry</h2>
+      <p>That page cannot be found</p>
+      <Link to="/">Back to the homepage...</Link>
+    </div>
    );
 }
  
