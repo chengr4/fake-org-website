a8d6c826c8334fda27529fae302f0aa6033e6528
Eddie Cheng <54136390+chengr4@users.noreply.github.com>
2021-04-20 17:00:05 +0800

    finish navbar

--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -1,13 +1,33 @@
-import { Link } from 'react-router-dom';
-const Navbar = () => {
-  return ( 
-    <nav className="navbar">
-      <h1>Vynca</h1>
-      <div className="links">
-        <Link to="/login">Login</Link>
-      </div>
-    </nav>
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Navbar = ({path}) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.appbar} color="transparent" elevation={0}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Vynca
+          </Typography>
+          <Button component={Link} to={"/"+path} color="secondary">{path}</Button>
+        </Toolbar>
+      </AppBar>
   );
-}
- 
+};
+
 export default Navbar;
